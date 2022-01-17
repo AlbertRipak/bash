a16bf5f36dae8599252030e4598ef68e32d6f83b
AlbertRipak <arspak107@gmail.com>
2022-01-17 16:58:37 +0200

    new changes

--- settingSystem/configureNewSystem.sh
+++ settingSystem/configureNewSystem.sh
@@ -14,22 +14,21 @@ sudo dnf update
 
 #sudo rpm -Uhv http://rpms.remirepo.net/enterprise/remi-release-8.rpm
 #sudo rpm -Uhv https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-sudo rpm -Uhv http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+rpm -Uhv http://rpms.remirepo.net/enterprise/remi-release-7.rpm
 rpm -Uhv https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 
-
-sudo yum -y install vim mc tar binutils java elfutils elfutils-libelf 
-sudo yum -y install gcc gcc-c++ glibc glibc-common glibc-devel cpp 
-sudo yum -y install make sysstat libaio libaio-devel unixODBC unixODBC-devel 
-sudo yum -y install net-tools bind-utils epel-release iscsi-initiator-utils 
-sudo yum -y install elfutils-libelf-devel network-scripts
-sudo yum -y install compat-lib* ksh iftop htop atop lsof 
-sudo yum -y install wget bzip2 traceroute gdisk unzip zip
-sudo yum -y install xorg-x11-utils*
-sudo yum -y install libnsl
-sudo yum config-manager --set-enabled powertools
-sudo yum -y xeyes
-sudo yum -y install xorg-x11-server-Xorg xorg-x11-xauth xorg-x11-apps
+yum -y install vim mc tar binutils java elfutils elfutils-libelf 
+yum -y install gcc gcc-c++ glibc glibc-common glibc-devel cpp 
+yum -y install make sysstat libaio libaio-devel unixODBC unixODBC-devel 
+yum -y install net-tools bind-utils epel-release iscsi-initiator-utils 
+yum -y install elfutils-libelf-devel network-scripts
+yum -y install compat-lib* ksh iftop htop atop lsof 
+yum -y install wget bzip2 traceroute gdisk unzip zip
+yum -y install xorg-x11-utils*
+yum -y install libnsl
+yum config-manager --set-enabled powertools
+yum -y xeyes
+yum -y install xorg-x11-server-Xorg xorg-x11-xauth xorg-x11-apps
 
 # Configure sshd
 ## Change new port
@@ -63,19 +62,19 @@ sudo mkdir -p /u01/app/oracle/product/11.2.0/dbhome_1
 sudo chown -R oracle:oinstall /u01
 
 echo "STEP 9: Setting grid profile!"
-sudo echo 'TMP=/tmp; export TMP
+echo 'TMP=/tmp; export TMP
 TMPDIR=$TMP; export TMPDIR
 ORACLE_HOSTNAME=$HOSTNAME; export ORACLE_HOSTNAME
-ORACLE_BASE=/u01/app/grid; export ORACLE_BASE
-ORACLE_HOME=$ORACLE_BASE/product/11.2.0/grid_home; export ORACLE_HOME
-ORACLE_SID=+ASM; export ORACLE_SID
+ORACLE_BASE=/u01/app/oracle; export ORACLE_BASE
+ORACLE_HOME=$ORACLE_BASE/product/11.2.0/dbhome_1; export ORACLE_HOME
+ORACLE_SID=orcl; export ORACLE_SID
 ORACLE_TERM=xterm; export ORACLE_TERM
 BASE_PATH=/usr/sbin:$PATH; export BASE_PATH
 PATH=$ORACLE_HOME/bin:$GRID_HOME/bin:$BASE_PATH; export PATH
 LD_LIBRARY_PATH=$ORACLE_HOME/lib:/lib:/usr/lib; export LD_LIBRARY_PATH
 CLASSPATH=$ORACLE_HOME/JRE:$ORACLE_HOME/jlib:$ORACLE_HOME/rdbms/jlib; export CLASSPATH
-' >> /home/grid/.bash_profile
-sudo chown -R grid:oinstall /home/grid/.bash_profile
+' >> /home/oracle/.bash_profile
+chown -R oracle:oinstall /home/oracle/.bash_profile
 
 echo "STEP 10: Upgrade kernel parameters!"
 sudo echo "
